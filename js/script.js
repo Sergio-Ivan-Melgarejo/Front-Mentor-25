3c72dc04d1bba8eb6690c4bf4921e62923e9815e
Sergio <sergio.ivan.melgarejo@gmail.com>
2022-01-28 22:29:47 -0300

    i added nav

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const logoMovil = document.getElementById("logo-movil");
+const navMovil = document.getElementById("nav-movil");
+const navUl = document.getElementById("nav-ul");
+
+logoMovil.addEventListener("click",()=>{
+    navMovil.classList.toggle("select");
+    logoMovil.classList.toggle("select");
+    if(logoMovil.classList.value.includes("select")) {
+        logoMovil.children[0].src = "./images/icon-hamburger.svg";
+    }
+    else logoMovil.children[0].src = "./images/icon-close.svg";
+    console.log(logoMovil.classList.value.includes("select"))
+})
+
+navUl.addEventListener("click",(e)=>{
+    const ele = e.target;
+    if (ele.classList.value.includes("header__li-box") ){
+        ele.parentNode.classList.toggle("select");
+    }
+    else if (ele.parentNode.classList.value.includes("header__li-box")){
+        ele.parentNode.parentNode.classList.toggle("select");
+    }
+})
+
